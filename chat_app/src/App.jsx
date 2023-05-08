2917108ffd9001709834a48d3f14cddcc0ad13e9
mahima23anu <111680119+mahima23anu@users.noreply.github.com>
2023-05-08 10:07:25 +0530

    Initial commit

---
+++ chat_app/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Logo from './Logo';
+import Form from './Form';
+import "./index.css"
+
+const App = () => {
+  return (
+    <div>
+      <Logo />
+      <Form />
+    </div>
+  );
+}
+
+export default App
